2de89de3e28ce6d92e888231ace6c4d477b8e3a3
webarebear107 <webarebear107@gmail.com>
2026-07-30 16:20:22 +0800

    commit

--- login_system.sql
+++ login_system.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2026 at 12:53 AM
+-- Generation Time: Jul 30, 2026 at 10:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
-
---Database: `login_system`
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -19,6 +17,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `login_system`
+--
 
 -- --------------------------------------------------------
 
@@ -33,7 +34,7 @@ CREATE TABLE `archive_masterlist` (
   `lastname` varchar(50) NOT NULL,
   `mi` varchar(3) NOT NULL,
   `gender` varchar(15) NOT NULL,
-  `bdate` varchar(50) NOT NULL,
+  `bdate` date NOT NULL,
   `strand` varchar(50) NOT NULL,
   `section` varchar(50) NOT NULL,
   `gradelvl` int(5) NOT NULL,
@@ -50,11 +51,11 @@ CREATE TABLE `archive_masterlist` (
 
 CREATE TABLE `attendance` (
   `id` int(11) NOT NULL,
-  `lrn` varchar(50) DEFAULT NULL,
+  `lrn` bigint(20) UNSIGNED NOT NULL,
   `fullname` varchar(150) DEFAULT NULL,
   `gender` varchar(10) NOT NULL,
   `section` varchar(50) NOT NULL,
-  `gradelvl` varchar(3) NOT NULL,
+  `gradelvl` int(5) NOT NULL,
   `contact` varchar(20) NOT NULL,
   `date_logged` date DEFAULT NULL,
   `time_logged` time DEFAULT NULL,
@@ -67,7 +68,7 @@ CREATE TABLE `attendance` (
 --
 
 INSERT INTO `attendance` (`id`, `lrn`, `fullname`, `gender`, `section`, `gradelvl`, `contact`, `date_logged`, `time_logged`, `is_notif`, `created_at`) VALUES
-(1, '194501140434', 'Philip James D. Inong', '', 'Ruby', '12', '09673289351', '2026-03-23', '11:48:46', 'N', '2026-03-23 03:48:46');
+(1, 194501140434, 'Philip James D. Inong', 'Male', 'Ruby', 12, '09673289351', '2026-03-23', '11:48:46', 'N', '2026-03-23 03:48:46');
 
 -- --------------------------------------------------------
 
@@ -82,7 +83,7 @@ CREATE TABLE `masterlist` (
   `lastname` varchar(50) NOT NULL,
   `mi` varchar(3) NOT NULL,
   `gender` varchar(15) NOT NULL,
-  `bdate` varchar(50) NOT NULL,
+  `bdate` date NOT NULL,
   `strand` varchar(50) NOT NULL,
   `section` varchar(50) NOT NULL,
   `gradelvl` int(5) NOT NULL,
@@ -96,8 +97,7 @@ CREATE TABLE `masterlist` (
 --
 
 INSERT INTO `masterlist` (`id`, `lrn`, `firstname`, `lastname`, `mi`, `gender`, `bdate`, `strand`, `section`, `gradelvl`, `address`, `contact`, `generated_code`) VALUES
-(1, 194501140434, 'Philip James', 'Inong', 'D', 'Male', '8/31/2008', 'TVL-ICT Programming', 'Ruby', 12, 'Donfelipe Larrazabal', '09673289351', '194501140434');
-
+(1, 194501140434, 'Philip James', 'Inong', 'D', 'Male', '2008-08-31', 'TVL-ICT Programming', 'Ruby', 12, 'Donfelipe Larrazabal', '09673289351', '194501140434');
 
 -- --------------------------------------------------------
 
@@ -118,7 +118,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 (1, '12345', '$2y$10$OudzcnMR8xhBkxkTYlydFekhihIKHpzn4y/WE42Fppi0NTrVLsveS', '2025-12-19 06:18:12'),
-(4, '1234', '$2y$10$zIoR.8Jp2EheDNh3lVXAPubnrqm9K6re05Mz9gmahCtOm/e4DTyIa', '2026-01-05 13:39:30');
+(4, '1234', '$2y$10$zIoR.8Jp2EheDNh3lVXAPubnrqm9K6re05Mz9gmahCtOm/e4DTyIa', '2026-01-05 13:39:30'),
+(5, '14785', '$2y$10$9kfWCqnI1.YQIBrUAgJeNej0KuIRiWd3DI9vwH/jbwU4kix7pn30q', '2026-07-30 08:17:29');
 
 --
 -- Indexes for dumped tables
@@ -141,8 +142,7 @@ ALTER TABLE `attendance`
 --
 ALTER TABLE `masterlist`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `lrn` (`lrn`),
-  ADD UNIQUE KEY `lrn_2` (`lrn`);
+  ADD UNIQUE KEY `lrn` (`lrn`);
 
 --
 -- Indexes for table `users`
@@ -165,19 +165,19 @@ ALTER TABLE `archive_masterlist`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `masterlist`
 --
 ALTER TABLE `masterlist`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
